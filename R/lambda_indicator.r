5dab9aa1906cf0daef998513a8bf1489d362cd92
AugustT <tomaug@ceh.ac.uk>
2016-09-19 15:17:18 +0100

    Fix bug in lambda_indicator example

--- R/lambda_indicator.r
+++ R/lambda_indicator.r
@@ -45,11 +45,14 @@
 #' iter = 500
 #' 
 #' # Build a random set of data
-#' myArray <- array(rnorm(n = nsp*nyr*iter,
-#'                  mean = 0.5,
-#'                  sd = 0.1),
-#'                  c(nsp, nyr, iter))
-#' 
+#' myArray <- array(data = rnorm(n = nsp*nyr*iter,
+#'                               mean = 0.5,
+#'                               sd = 0.1),
+#'                  dim = c(nsp, nyr, iter),
+#'                  dimnames = list(paste0('SP',1:nsp),
+#'                                  1:nyr,
+#'                                  1:iter))
+#'
 #' # Ensure values are bounded by 0 and 1
 #' myArray[myArray > 1] <- 1
 #' myArray[myArray < 0] <- 0
