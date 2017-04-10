5f43031dc065374f515df65a2a1d5744ed5d21e5
AugustT <tomaug@ceh.ac.uk>
2017-04-10 11:19:13 +0100

    Add in BMA to the code base anfd the vignette

--- R/bayesian_meta_analysis.R
+++ R/bayesian_meta_analysis.R
@@ -3,11 +3,20 @@
 #' @description Use a bayesian meta-analysis to create an indicator from species index values and standard error.
 #' 
 #' @param data a data.frame with 4 columns in this order: species, year, index, se (standard error) 
-#' @param randwalk If \code{TRUE} a random walk feature is added to the model (defualt), else years are treated as independent.
 #' @param plot Logical, should a trace plot be plotted?
+#' @param model The type of model to be used. The default of 'random_walk' is strongly recommended.
+#' Two other models 'uniform' and 'uniform_noeta' exist but are almost always inferior.
+#' @param parallel if \code{TRUE} the model chains will be run in parallel using one fewer cores than
+#' are availbale on the computer.
+#' @param incl.model if \code{TRUE} the model is added as an attribute of the object returned
+#' @param n.iter The number of iterations of the model to run. Defaults to 10,000 to avoid long run times
+#' though much longer runs are usually required to reach convergence
+#' @param m.scale The measurement scale of the data. The scale of the data is assumed to be logarithmic.
+#' Here you specify which log scale the data is on ('loge', 'log10', or 'logit'). Defaults to 'loge'.
 #' @return Returns a dataframe with 4 columns: Year, Index, lower2.5, upper97.5. The last two columns are the credible intervals
-#' @import R2jags
 #' @import reshape2
+#' @import jagsUI
+#' @importFrom boot inv.logit
 #' @importFrom coda mcmc.list as.mcmc
 #' @export
 #' @examples 
@@ -26,78 +35,95 @@
 #'                CIs = bma_indicator[,c(3,4)])
 
 
-bma <- function(data, randwalk = TRUE, plot = TRUE) {
-
-  if(!identical(colnames(data), c('species', 'year', 'index', 'se'))){
+bma <- function (data,
+                 plot = TRUE,
+                 model = 'random_walk',
+                 parallel = FALSE,
+                 incl.model = TRUE,
+                 n.iter = 1e4,
+                 m.scale = 'loge'){
+  
+  if (!identical(colnames(data), c("species", "year", "index", 
+                                   "se"))) {
     stop('data column names should be: "species", "year", "index", "se"')
   }
   
-  # Create 2 matricies, for index and se
-  index <- log(acast(na.omit(data), species ~ year, value.var='index'))
-  se <- acast(na.omit(data), species ~ year, value.var='se')
-
-  # convert to a format for bugs
-  bugs_data <- list(Nsp = nrow(index),
-                    Nyr = ncol(index),
-                    estimate = index,
-                    se =  se)
-
-  #setup bugs
-  params = c('tau.b0', 'tau.eta', 'tau.I', 'I', 'alpha')
+  # This is not my preferrred behaviour
+  if(!m.scale %in% c('loge', 'log10', 'logit')) stop("m.scale must be 'loge', 'log10', or 'logit'")
   
-  # run model demending on user specification
-  if(randwalk){
-    model <- jags(data = bugs_data,
-                  inits = NULL,
-                  param = params, 
-                  model.file = bma_indicator(),
-                  n.chains = 3,
-                  n.thin = 2,
-                  n.iter = 10000)
-  } else {
-    model <- jags(data = bugs_data,
-                  inits = NULL,
-                  param = params, 
-                  model.file = bma_indicator_randwalk(),
-                  n.chains = 3,
-                  n.thin = 2,
-                  n.iter = 10000)
-  }
-
+  # pick the correct model
+  switch(tolower(model),
+         random_walk = {bugs_path <- bma_model_ranwalk()},
+         uniform = {bugs_path <- bma_model_uniform()},
+         unifoorm_noeta = {bugs_path <- bma_model_uniform_noeta()},
+         {stop("model type not know. Must be one of 'random_walk', 'uniform', 'uniform_noeta'")})
   
-  if(plot){
-    array_sim <- model$BUGSoutput$sims.array
-    comb.samples <- coda::mcmc.list(
-      lapply(1:3,
-             FUN = function(x, array_sim){
-               year_ests <- colnames(array_sim[,x,])[grepl('^I\\[', colnames(array_sim[,x,]))]
-               ar_temp <- array_sim[,x,c(head(year_ests, 1), tail(year_ests, 1))]
-               colnames(ar_temp) <- c('First year', 'Last year')
-               coda::as.mcmc(ar_temp)
-               },
-             array_sim = array_sim)
-    )
+  #index <- log(acast(na.omit(data), species ~ year, value.var = "index"))
+  #se <- acast(na.omit(data), species ~ year, value.var = "se")
+  
+  # 24 Feb - the index is already on the log scale (for butteflies at least)
+  #index <- log(acast(data, species ~ year, value.var = "index"))
+  index <- (acast(data, species ~ year, value.var = "index"))
+  
+  se <- acast(data, species ~ year, value.var = "se")
+  
+  bugs_data <- list(Nsp = nrow(index), Nyr = ncol(index), estimate = index, 
+                    se = se)
+  
+  params = c("tau.b0", "tau.eta", "tau.I", "logI", "alpha")
+  
+  model <- jagsUI::jags(data = bugs_data,
+                        inits = NULL,
+                        param = params,
+                        parallel = parallel,
+                        n.cores = parallel::detectCores()-1,
+                        model.file = bugs_path,
+                        n.chains = 3,
+                        n.thin = 2,
+                        n.iter = n.iter,
+                        n.burnin = floor(n.iter/2))
+  
+  if (plot) {
+    array_sim <- model$samples
+    comb.samples <- mcmc.list(lapply(1:3, FUN = function(x, 
+                                                         array_sim) {
+      year_ests <- colnames(array_sim[[x]])[grepl("^logI\\[",  # changed from I
+                                                  colnames(array_sim[[x]]))]
+      ar_temp <- array_sim[[x]][,c(head(year_ests, 1), tail(year_ests,1))]
+      colnames(ar_temp) <- c("First year", "Last year")
+      as.mcmc(ar_temp)
+    }, array_sim = array_sim))
     plot(comb.samples)
   }
   
+  pd <- data.frame(mean = unlist(model$mean),
+                   q2.5 = unlist(model$q2.5),
+                   q97.5 = unlist(model$q97.5))
   
-  pd <- model$BUGSoutput$summary[,c(1,3,7)]
+  # convert the logI back to the measurement scale  
+  unb2b <- function(x, m.scale){
+    switch(m.scale, 
+           loge = x <- exp(x),
+           log10 = x <- 10^x,
+           logit = {x <- boot::inv.logit(x)},
+           warning(paste(m.scale, 'unknown, no back-transformation applied')))
+    return(x)
+  }
+  pd <- unb2b(pd[grepl("^logI",dimnames(pd)[[1]]),], m.scale)
   
-  # take the raw output from model and convert it into an indicator,
-  # with options for centering [not implemented]
-  rescale_bayesian_indicator <- function(x, centering = 'firstyr') {
-    if(centering == 'firstyr'){
-      x <- x/x[1,1]
-    } 
+  rescale_bayesian_indicator <- function(x, centering = "firstyr") {
+    if (centering == "firstyr") {
+      x <- x/x[1, 1]
+    }
     100 * x
   }
-
-  pd <- rescale_bayesian_indicator(pd, centering='firstyr')
-
-  pd <- dcast(melt(pd[grepl('^I',dimnames(pd)[[1]]),]), Var1~Var2)
-  names(pd) <- c('Year', 'Index', 'lower2.5', 'upper97.5')
-  pd$Year  <- as.numeric(pd$Year)
-
+  pd <- rescale_bayesian_indicator(pd, centering = "firstyr")
+  pd <- dcast(melt(as.matrix(pd)), Var1 ~ 
+                Var2)
+  names(pd) <- c("Year", "Index", "lower2.5", "upper97.5")
+  pd$Year <- as.numeric(pd$Year)
+  
+  if(incl.model) attr(pd, 'model') <- model
+  
   return(pd)
-
 }
--- R/bma_BUGS_models.R
+++ R/bma_BUGS_models.R
@@ -1,54 +1,10 @@
-bma_indicator <- function(temp_file = tempfile()){
+bma_model_ranwalk <- function(temp_file = tempfile()){
   
-  model <- "
+  model <- '
 model {
-  # Priors 
-  alpha ~ dnorm(0, 0.0001)
+# Priors 
   
-  # one value per species
-  for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
-  
-  # one value per year
-  for (t in 1:Nyr){logI[t] ~ dnorm(0, tau.I)}
-  
-  # one value per site-species
-  for (i in 1:Nsp){   
-    for (t in 1:Nyr){
-      eta[i,t] ~ dnorm(0, tau.eta) # process error
-      se[i,t] ~ dunif(0,1) # for the missing values
-    }}
-  
-  # Hyperpriors
-  tau.b0 ~ dt(0,1,1)T(0,)
-  tau.I ~ dt(0,1,1)T(0,)
-  tau.eta ~ dt(0,1,1)T(0,)
-  
-  # Each year-species combos is estimated with error
-  for (t in 1:Nyr){
-    for (i in 1:Nsp){ 
-      estimate[i,t] ~ dnorm(muN[i,t], tau.psd[i,t]) # Estimated effect size
-      tau.psd[i,t] <- pow(se[i,t], -2)    # 'Known' measurement error
-      
-      #muN is the true unknown species index this year (on the log scale)
-      # it's a simple linear function of the year and site effects with process error
-      muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
-    }}
-  
-  # derived parameter: indicator on the measurement scale
-  for (t in 1:Nyr){log(I[t]) <- logI[t]}
-}"
-  
-  writeLines(text = model, con = temp_file)
-  return(temp_file)
-}
-  
-bma_indicator_ranwalk <- function(temp_file = tempfile()){
-  
-  model <- "
-model {
-  
-  # Priors 
-  alpha ~ dnorm(0, 0.0001)
+  alpha ~ dnorm(0, 0.001)
   
   # one value per species
   for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
@@ -73,17 +29,114 @@ model {
   for (t in 1:Nyr){
   for (i in 1:Nsp){ 
   estimate[i,t] ~ dnorm(muN[i,t], tau.psd[i,t]) # Estimated effect size
-  tau.psd[i,t] <- pow(se[i,t], -2)    # 'Known' measurement error
+  tau.psd[i,t] <- pow(se[i,t], -2)    # Known measurement error
   
   #muN is the true unknown species index this year (on the log scale)
-  # it's a simple linear function of the year and site effects, with 'process error'
-  muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
+  # its a simple linear function of the year and site effects, with "process error"
+  muN[i,t] <- b0[i] + logI[t] + eta[i,t]
+  #muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
   }}
   
   # derived parameter: indicator on the measurement scale
-  for (t in 1:Nyr){log(I[t]) <- logI[t]}
+  #for (t in 1:Nyr){log(I[t]) <- logI[t]}
   
-}"
+}'
+  
+  writeLines(text = model, con = temp_file)
+  return(temp_file)
+}
+
+
+bma_model_uniform <- function(temp_file = tempfile()){
+  
+  model <- '
+  model {
+  # Priors 
+  alpha ~ dnorm(0, 0.001)
+  
+  # one value per species
+  for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
+  
+  # one value per year
+  for (t in 1:Nyr){logI[t] ~ dunif(-10, 10)}
+  
+  # one value per site-species
+  for (i in 1:Nsp){   
+  for (t in 1:Nyr){
+  eta[i,t] ~ dnorm(0, tau.eta) # process error
+  se[i,t] ~ dunif(0,1) # for the missing values
+  }}
+  
+  # Hyperpriors
+  tau.b0 ~ dt(0,1,1)T(0,) # redundant if species have already been standardised
+  tau.I ~ dt(0,1,1)T(0,) # obsolete in this model, but retained for compatability
+  tau.eta ~ dt(0,1,1)T(0,)
+  
+  # Each year-species combos is estimated with error
+  for (t in 1:Nyr){
+  for (i in 1:Nsp){ 
+  estimate[i,t] ~ dnorm(muN[i,t], tau.psd[i,t]) # Estimated effect size
+  tau.psd[i,t] <- pow(se[i,t], -2)    # Known measurement error
+  
+  #muN is the true unknown species index this year (on the log scale)
+  # its a simple linear function of the year and site effects, with "process error"
+  muN[i,t] <- b0[i] + logI[t] + eta[i,t]
+  #muN[i,t] <- b0[i] + logI[t]
+  #muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
+  }}
+
+  # derived parameter: indicator on the measurement scale
+  #for (t in 1:Nyr){log(I[t]) <- logI[t]}
+  
+  }'
+  
+  writeLines(text = model, con = temp_file)
+  return(temp_file)
+}
+
+
+bma_model_uniform_noeta <- function(temp_file = tempfile()){
+  
+  model <- '
+  model {
+  # Priors 
+  alpha ~ dnorm(0, 0.001)
+  
+  # one value per species
+  for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
+  
+  # one value per year
+  for (t in 1:Nyr){logI[t] ~ dunif(-10, 10)}
+  
+  # one value per site-species
+  for (i in 1:Nsp){   
+  for (t in 1:Nyr){
+  eta[i,t] ~ dnorm(0, tau.eta) # process error
+  se[i,t] ~ dunif(0,1) # for the missing values
+  }}
+  
+  # Hyperpriors
+  tau.b0 ~ dt(0,1,1)T(0,) # redundant if species have already been standardised
+  tau.I ~ dt(0,1,1)T(0,) # obsolete in this model, but retained for compatability
+  tau.eta ~ dt(0,1,1)T(0,)
+  
+  # Each year-species combos is estimated with error
+  for (t in 1:Nyr){
+  for (i in 1:Nsp){ 
+  estimate[i,t] ~ dnorm(muN[i,t], tau.psd[i,t]) # Estimated effect size
+  tau.psd[i,t] <- pow(se[i,t], -2)    # Known measurement error
+  
+  #muN is the true unknown species index this year (on the log scale)
+  # its a simple linear function of the year and site effects, with "process error"
+  #muN[i,t] <- b0[i] + logI[t] + eta[i,t]
+  muN[i,t] <- b0[i] + logI[t]
+  #muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
+  }}
+
+  # derived parameter: indicator on the measurement scale
+  #for (t in 1:Nyr){log(I[t]) <- logI[t]}
+
+  }'
   
   writeLines(text = model, con = temp_file)
   return(temp_file)
