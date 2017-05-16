3e805b5ee4fedcd81c5e6791818836061a9c5884
GPowney <g.powney@googlemail.com>
2017-05-16 16:35:50 +0100

    Test: model prior changes

--- R/bma_BUGS_models.R
+++ R/bma_BUGS_models.R
@@ -4,10 +4,12 @@ bma_model_ranwalk <- function(temp_file = tempfile()){
 model {
 # Priors 
   
-  alpha ~ dnorm(0, 0.001)
+  #alpha0 ~ dnorm(0, 0.001)
+  alpha.b0 ~ dnorm(0, 0.001)
   
   # one value per species
-  for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
+  for (i in 1:Nsp){b0[i] ~ dnorm(alpha.b0, tau.b0)}
+  #for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
   
   # one value per year
   logI[1] ~ dnorm(0, 0.0001)
@@ -33,8 +35,8 @@ model {
   
   #muN is the true unknown species index this year (on the log scale)
   # its a simple linear function of the year and species effects, with "process error"
-  #muN[i,t] <- b0[i] + logI[t] + eta[i,t]
-  muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
+  muN[i,t] <- b0[i] + logI[t] + eta[i,t]
+  #muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
   }}
   
   # derived parameter: indicator on the measurement scale
