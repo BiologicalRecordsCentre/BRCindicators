557311649af5b56411d4628c9c1500cdbbaa2ab8
AugustT <tomaug@ceh.ac.uk>
2017-11-22 17:28:07 +0000

    Starting to add the new versions of the BMAs, i still have errors for the smoothed ones because X is not known

--- R/bayesian_meta_analysis.R
+++ R/bayesian_meta_analysis.R
@@ -4,8 +4,7 @@
 #' 
 #' @param data a data.frame with 4 columns in this order: species, year, index, se (standard error) 
 #' @param plot Logical, should a trace plot be plotted?
-#' @param model The type of model to be used. The default of 'random_walk' is strongly recommended.
-#' Two other models 'uniform' and 'uniform_noeta' exist but are almost always inferior.
+#' @param model The type of model to be used. See details.
 #' @param parallel if \code{TRUE} the model chains will be run in parallel using one fewer cores than
 #' are availbale on the computer.
 #' @param incl.model if \code{TRUE} the model is added as an attribute of the object returned
@@ -13,6 +12,16 @@
 #' though much longer runs are usually required to reach convergence
 #' @param m.scale The measurement scale of the data. The scale of the data is assumed to be logarithmic.
 #' Here you specify which log scale the data is on ('loge', 'log10', or 'logit'). Defaults to 'loge'.
+#' @param num.knots If using either of the smooth models this specifies the number of knots.
+#' @details There are a number of model to choose from:
+#' \itemize{
+#'  \item{\code{"random_walk"}}{ - Also known as BMA3, strongly recommended.}
+#'  \item{\code{"uniform"}}{ - Also known as BMA2.}
+#'  \item{\code{"uniform_noeta"}}{ - Also known as BMA1.}
+#'  \item{\code{"FNgr"}}{ - Indicator defined by Growth rates, as in Freeman & Newson.}
+#'  \item{\code{"smooth_stoch"}}{ - Indicator defined by Growth rates, with Ruppert smoother (stochastic version).}
+#'  \item{\code{"smooth_det"}}{ - Indicator defined by Growth rates, with Ruppert smoother (deterministic version).}
+#' }
 #' @return Returns a dataframe with 4 columns: Year, Index, lower2.5, upper97.5. The last two columns are the credible intervals
 #' @import reshape2
 #' @import jagsUI
@@ -41,7 +50,8 @@ bma <- function (data,
                  parallel = FALSE,
                  incl.model = TRUE,
                  n.iter = 1e4,
-                 m.scale = 'loge'){
+                 m.scale = 'loge',
+                 num.knots = 3){
   
   if (!identical(colnames(data), c("species", "year", "index", 
                                    "se"))) {
@@ -56,10 +66,12 @@ bma <- function (data,
          random_walk = {bugs_path <- bma_model_ranwalk()},
          uniform = {bugs_path <- bma_model_uniform()},
          uniform_noeta = {bugs_path <- bma_model_uniform_noeta()},
-         {stop("model type not know. Must be one of 'random_walk', 'uniform', 'uniform_noeta'")})
-  
-  #index <- log(acast(na.omit(data), species ~ year, value.var = "index"))
-  #se <- acast(na.omit(data), species ~ year, value.var = "se")
+         fngr = {bugs_path <- bma_model_FNgr()},
+         smooth_stoch = {bugs_path <- bma_model_smooth_stoch()},
+         smooth_det = {bugs_path <- bma_model_smooth_det()},
+         {stop(paste("model type not know. Must be one of 'random_walk',",
+                     "'uniform', 'uniform_noeta', 'FNgr', 'smooth_stoch',",
+                     "'smooth_det'"))})
   
   # 24 Feb - the index is already on the log scale (for butteflies at least)
   #index <- log(acast(data, species ~ year, value.var = "index"))
@@ -67,10 +79,25 @@ bma <- function (data,
   
   se <- acast(data, species ~ year, value.var = "se")
   
-  bugs_data <- list(Nsp = nrow(index), Nyr = ncol(index), estimate = index, 
-                    se = se, max_se = max(se, na.rm = TRUE)) 
+  # Setup BUGS data
+  bugs_data <- list(nsp = nrow(index),
+                    nyears = ncol(index),
+                    estimate = index, 
+                    sigma.obs = se, 
+                    max_se = max(se, na.rm = TRUE)) 
   
-  params = c("tau.b0", "tau.eta", "tau.I", "logI", "alpha")
+  if(model %in% c('smooth_stoch', 'smooth_det')){
+    bugs_data[['Z']] <- makeZ(num.knots = num.knots,
+                              covariate = seq(min(data$year),
+                                              max(data$year)))
+    bugs_data[['num.knots']] <- num.knots
+  }
+  
+  # Setup parameters to monitor
+  params = c("tau.spi", "logI", "spindex", "sigma.obs")
+  if(model %in% c('smooth_stoch', 'smooth_det')) params <- c(params, "logI.raw")
+  if(model %in% c('random_walk', 'uniform', 'uniform_noeta')) params <- c(params, "tau.eta")
+  if(model %in% c('random_walk')) params <- c(params, "tau.I")
   
   model <- jagsUI::jags(data = bugs_data,
                         inits = NULL,
--- R/bma_BUGS_models.R
+++ R/bma_BUGS_models.R
@@ -1,143 +1,402 @@
+
+# Multispecies indicators from Bayesian Meta-Analysis 
+# Each function contains BUGS code for a different model
+# Code by Nick Isaac, Tom August & Steve Freeman
+
+################################################################################
+
+# 15/11/17: There are six models, all with the prefix "bma_model"
+  # the ranwalk, uniform & uniform_noeta were written by Nick Isaac
+  # FNgr, SmoothStoch & SmoothDet were written by Stephen Freeman
+  # Isaac and Freeman used different syntax: I have harmonized some terms
+    # Terms in Freeman's code that have been changed to match syntax in Isaac's code:
+      # logI is used for the multispecies indicator on the log scale (in place of 'tindicator' or 'sindicator')
+      # logLambda is used for the multispecies log growth rate (in place of 'growth')
+      # estimate is used for the estimated log abundance (in place of 'species')
+      # tau.sg used instead of taus
+    # Terms in Isaac's code that have been changed to match syntax in Freeman's code:
+      # spindex used for the true unknown state variable (in place of "muN")
+      # s used for species loop (in place of "i")
+      # nyr and nsp used in place of Nyr & Nsp
+    # Terms changed from both
+      # sigma.obs used for the "known" measurement error (formerly sigma in Freeman, se in Isaac)
+      # tau.obs used for 1/sigma.obs^2 (formerly tau in Freeman, tau.psd in Isaac)
+    # Other changes
+      # sigma.obs is allowed to be data, but can be omitted and imputed from prior (as in Isaac's code)
+      # variances (sigma2) removed from Freeman code
+
+    # outstanding issues: 
+    # Isaac code uses half Cauchy priors for precisions, Freeman uses uniform on stdvs
+    # Isaac code does not calculate stdvs from precisions
+# TA additions
+  # tau.obs changed to tau.obs[s,t] in new 3 models [errors otherwise]
+  # undocumented change tau.b0 -> tau.spi
+
+################################################################################
+
 bma_model_ranwalk <- function(temp_file = tempfile()){
+  # Also known as BMA3
   
   model <- '
 model {
-# Priors 
-  
-  alpha ~ dnorm(0, 0.001)
+  ###################  Define priors
   
   # one value per species
-  for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
+  for (s in 1:nsp){b0[s] ~ dnorm(0, tau.spi)}
   
   # one value per year
   logI[1] ~ dnorm(0, 0.0001)
-  for (t in 2:Nyr){logI[t] ~ dnorm(logI[t-1], tau.I)}
+  for (t in 2:nyears){logI[t] ~ dnorm(logI[t-1], tau.I)}
   
   # one value per site-species
-  for (i in 1:Nsp){   
-  for (t in 1:Nyr){
-  eta[i,t] ~ dnorm(0, tau.eta) # process error
-  se[i,t] ~ dunif(0,max_se) # for the missing values
+  for (s in 1:nsp){   
+    for (t in 1:nyears){
+      eta[s,t] ~ dnorm(0, tau.eta) # process error
+      sigma.obs[s,t] ~ dunif(0, max_se) # for the missing values
   }}
   
   # Hyperpriors
-  tau.b0 ~ dt(0,1,1)T(0,)
+  tau.spi ~ dt(0,1,1)T(0,)
   tau.I ~ dt(0,1,1)T(0,)
   tau.eta ~ dt(0,1,1)T(0,)
-  
+
+  ###################  Define likelihood  #######################
+
   # Each year-species combos is estimated with error
-  for (t in 1:Nyr){
-  for (i in 1:Nsp){ 
-  estimate[i,t] ~ dnorm(muN[i,t], tau.psd[i,t]) # Estimated effect size
-  tau.psd[i,t] <- pow(se[i,t], -2)    # Known measurement error
+  for (t in 1:nyears){
+    for (s in 1:nsp){ 
+      estimate[s,t] ~ dnorm(spindex[s,t], tau.obs[s,t]) # Estimated effect size
+      tau.obs[s,t] <- pow(sigma.obs[s,t], -2)    # Known measurement error
   
-  #muN is the true unknown species index this year (on the log scale)
+  #spindex is the true unknown species index this year (on the log scale)
   # its a simple linear function of the year and species effects, with "process error"
-  muN[i,t] <- b0[i] + logI[t] + eta[i,t]
-  #muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
+  spindex[s,t] <- b0[s] + logI[t] + eta[s,t]
   }}
-  
-  # derived parameter: indicator on the measurement scale
-  #for (t in 1:Nyr){log(I[t]) <- logI[t]}
-  
+
+  #########################  end likelihood ###########################
+
 }'
   
   writeLines(text = model, con = temp_file)
   return(temp_file)
 }
 
+################################################################################
 
 bma_model_uniform <- function(temp_file = tempfile()){
+  # Also known as BMA2
   
   model <- '
   model {
-  # Priors 
-  alpha ~ dnorm(0, 0.001)
-  
+  ###################  Define priors
+
   # one value per species
-  for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
+  for (s in 1:nsp){b0[s] ~ dnorm(0, tau.spi)}
   
   # one value per year
-  for (t in 1:Nyr){logI[t] ~ dunif(-10, 10)}
+  for (t in 1:nyears){logI[t] ~ dunif(-10, 10)}
   
   # one value per site-species
-  for (i in 1:Nsp){   
-  for (t in 1:Nyr){
-  eta[i,t] ~ dnorm(0, tau.eta) # process error
-  se[i,t] ~ dunif(0,max_se) # for the missing values
+  for (s in 1:nsp){   
+    for (t in 1:nyears){
+      eta[s,t] ~ dnorm(0, tau.eta) # process error
+      sigma.obs[s,t] ~ dunif(0, max_se) # for the missing values
   }}
   
   # Hyperpriors
-  tau.b0 ~ dt(0,1,1)T(0,) # redundant if species have already been standardised
-  tau.I ~ dt(0,1,1)T(0,) # obsolete in this model, but retained for compatability
+  tau.spi ~ dt(0,1,1)T(0,) # redundant if species have already been standardised
   tau.eta ~ dt(0,1,1)T(0,)
+
+  ###################  Define likelihood  #######################
   
   # Each year-species combos is estimated with error
-  for (t in 1:Nyr){
-  for (i in 1:Nsp){ 
-  estimate[i,t] ~ dnorm(muN[i,t], tau.psd[i,t]) # Estimated effect size
-  tau.psd[i,t] <- pow(se[i,t], -2)    # Known measurement error
+  for (t in 1:nyears){
+    for (s in 1:nsp){ 
+      estimate[s,t] ~ dnorm(spindex[s,t], tau.obs[s,t]) # Estimated effect size
+      tau.obs[s,t] <- pow(sigma.obs[s,t], -2)    # Known measurement error
   
-  #muN is the true unknown species index this year (on the log scale)
+  #spindex is the true unknown species index this year (on the log scale)
   # its a simple linear function of the year and species effects, with "process error"
-  muN[i,t] <- b0[i] + logI[t] + eta[i,t]
-  #muN[i,t] <- b0[i] + logI[t]
-  #muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
+  spindex[s,t] <- b0[s] + logI[t] + eta[s,t]
   }}
 
-  # derived parameter: indicator on the measurement scale
-  #for (t in 1:Nyr){log(I[t]) <- logI[t]}
-  
+  #########################  end likelihood ###########################
+
   }'
   
   writeLines(text = model, con = temp_file)
   return(temp_file)
 }
 
+################################################################################
 
 bma_model_uniform_noeta <- function(temp_file = tempfile()){
+  # Also known as BMA1
   
   model <- '
   model {
-  # Priors 
-  alpha ~ dnorm(0, 0.001)
-  
+  ###################  Define priors
+
   # one value per species
-  for (i in 1:Nsp){b0[i] ~ dnorm(0, tau.b0)}
+  for (s in 1:nsp){b0[s] ~ dnorm(0, tau.spi)}
   
   # one value per year
-  for (t in 1:Nyr){logI[t] ~ dunif(-10, 10)}
+  for (t in 1:nyears){logI[t] ~ dunif(-10, 10)}
   
   # one value per site-species
-  for (i in 1:Nsp){   
-  for (t in 1:Nyr){
-  eta[i,t] ~ dnorm(0, tau.eta) # process error
-  se[i,t] ~ dunif(0,max_se) # for the missing values
+  for (s in 1:nsp){   
+    for (t in 1:nyears){
+      eta[s,t] ~ dnorm(0, tau.eta) # process error
+      sigma.obs[s,t] ~ dunif(0, max_se) # for the missing values
   }}
   
   # Hyperpriors
-  tau.b0 ~ dt(0,1,1)T(0,) # redundant if species have already been standardised
-  tau.I ~ dt(0,1,1)T(0,) # obsolete in this model, but retained for compatability
+  tau.spi ~ dt(0,1,1)T(0,) # redundant if species have already been standardised
   tau.eta ~ dt(0,1,1)T(0,)
-  
+
+  ###################  Define likelihood  #######################
+
   # Each year-species combos is estimated with error
-  for (t in 1:Nyr){
-  for (i in 1:Nsp){ 
-  estimate[i,t] ~ dnorm(muN[i,t], tau.psd[i,t]) # Estimated effect size
-  tau.psd[i,t] <- pow(se[i,t], -2)    # Known measurement error
+  for (t in 1:nyears){
+    for (s in 1:nsp){ 
+      estimate[s,t] ~ dnorm(spindex[s,t], tau.obs[s,t]) # Estimated effect size
+      tau.obs[s,t] <- pow(sigma.obs[s,t], -2)    # Known measurement error
   
-  #muN is the true unknown species index this year (on the log scale)
+  #spindex is the true unknown species index this year (on the log scale)
   # its a simple linear function of the year and species effects, with "process error"
-  #muN[i,t] <- b0[i] + logI[t] + eta[i,t]
-  muN[i,t] <- b0[i] + logI[t]
-  #muN[i,t] <- alpha + b0[i] + logI[t] + eta[i,t]
+  spindex[s,t] <- b0[s] + logI[t]
   }}
 
-  # derived parameter: indicator on the measurement scale
-  #for (t in 1:Nyr){log(I[t]) <- logI[t]}
+  #########################  end likelihood ###########################
 
   }'
   
   writeLines(text = model, con = temp_file)
   return(temp_file)
 }
+
+################################################################################
+
+bma_model_FNgr <- function(temp_file = tempfile()){
+  # Indicator defined by Growth rates, as in Freeman & Newson
+  # Defined by equations 1-5 in Steve Freeman's document of 2/11/17
+  # Also known as "FN_indicator" in Steve's email of 2/11/17
+  
+  model <- '
+  model {
+
+  ###################  Define priors
+  # process errors
+  tau.spi <- pow(sigma.spi,-2)
+  sigma.spi ~ dunif(0,30)
+  tau.sg <- pow(sigma.sg,-2)
+  sigma.sg ~ dunif(0,1000)
+
+  # observation errors
+  # one value per site-species
+  for (s in 1:nsp){   
+    for (t in 1:nyears){
+      sigma.obs[s,t] ~ dunif(0, max_se) # for the missing values
+    }}
+
+  for (s in 1:nsp){
+    spindex[s,1] ~ dnorm(logI[1], tau.spi)
+  }
+  
+  for (t in 1:(nyears)){
+    logI[t] ~ dnorm(0,0.000001)
+  }
+  
+  ###################  Define likelihood  #######################
+  
+  for (t in 1:(nyears-1)){logLambda[t] <- logI[t+1] - logI[t]}
+  
+  for (s in 1:nsp){
+    for (t in 1:(nyears-1)){
+      spgrowth[s,t] ~ dnorm(logLambda[t], tau.sg)
+  }}
+  
+  for (s in 1:nsp){
+    for (t in 2:(nyears)){
+      spindex[s,t] <- spindex[s,1] + sum(spgrowth[s,1:(t-1)])
+  }}
+  
+  for (s in 1:nsp){
+    for (t in 1:(nyears)){
+      estimate[s,t] ~ dnorm(spindex[s,t], tau.obs[s,t])
+      tau.obs[s,t] <- pow(sigma.obs[s,t], -2)
+  }}
+  
+  
+  #########################  end likelihood ###########################
+
+}'
+  
+  writeLines(text = model, con = temp_file)
+  return(temp_file)
+}
+
+################################################################################
+
+bma_model_smooth_stoch <- function(temp_file = tempfile()){
+  # Indicator defined by Growth rates, with Ruppert smoother (stochastic version)
+  # Defined by equation 6 in Steve Freeman's document of 2/11/17
+  # Also known as "smooth_indicator_2" in Steve's email of 2/11/17
+  
+  model <- '
+  model {
+
+  ###################  Define priors
+  # process errors
+  tau.spi <- pow(sigma.spi,-2)
+  sigma.spi ~ dunif(0,30)
+  tau.sg <- pow(sigma.sg,-2)
+  sigma.sg ~ dunif(0,1000)
+
+  # observation errors
+  # one value per site-species
+  for (s in 1:nsp){   
+    for (t in 1:nyears){
+      sigma.obs[s,t] ~ dunif(0, max_se) # for the missing values
+  }}
+  
+  for (s in 1:nsp){
+    spindex[s,1] ~ dnorm(logI.raw[1], tau.spi)
+  }
+  
+  ########### Smoothing done here   #############
+  
+  beta[1]~dnorm(0,0.000001)
+  beta[2]~dnorm(0,0.000001)
+  taueps ~ dgamma(0.000001,0.000001)
+  taub ~ dgamma(0.000001,0.000001)
+  for(k in 1:num.knots){b[k] ~ dnorm(0,taub)}
+  
+  for (t in 1:(nyears)){
+    logI.raw[t] ~ dnorm(m[t], taueps)
+    m[t] <- mfe[t] + mre[t]
+    mfe[t] <- beta[1] * X[t,1] + beta[2] * X[t,2]
+    for (k in 1:num.knots){
+      temp[t,k]<-b[k]*Z[t,k]
+    }
+    mre[t] <- sum(temp[t,1:num.knots])
+  }
+  
+  ###################  Define likelihood  #######################
+  
+  for (t in 1:(nyears-1)){
+  logLambda[t] <- logI.raw[t+1] - logI.raw[t]
+  }
+  
+  for (s in 1:nsp){
+    for (t in 1:(nyears-1)){
+      spgrowth[s,t] ~ dnorm(logLambda[t], tau.sg)
+  }}
+  
+  for (s in 1:nsp){
+    for (t in 2:(nyears)){
+      spindex[s,t] <- spindex[s,1] + sum(spgrowth[s,1:(t-1)])
+  }}
+  
+  for (s in 1:nsp){
+    for (t in 1:(nyears)){
+      estimate[s,t] ~ dnorm(spindex[s,t], tau.obs[s,t])
+      tau.obs[s,t] <- pow(sigma.obs[s,t], -2)
+  }}
+  
+  logI <- m
+  
+  #########################  end likelihood ###########################
+  
+  
+}
+
+  '
+  
+  writeLines(text = model, con = temp_file)
+  return(temp_file)
+}
+
+################################################################################
+
+bma_model_smooth_det <- function(temp_file = tempfile()){
+  # Indicator defined by Growth rates, with Ruppert smoother (deterministic version)
+  # Defined by equation 7 in Steve Freeman's document of 2/11/17
+  # Also known as "smooth_indicator_1" in Steve's email of 2/11/17
+
+  model <- '
+  model {
+
+  ###################  Define priors
+  # process errors
+  tau.spi<-pow(sigma.y,-2)
+  sigma.y~dunif(0,30)
+  tau.sg<-pow(sigma.s,-2)
+  sigma.s~dunif(0,1000)
+
+  # observation errors
+  # one value per site-species
+  for (s in 1:nsp){   
+    for (t in 1:nyears){
+      sigma.obs[s,t] ~ dunif(0, max_se) # for the missing values
+    }}
+  
+  for (s in 1:nsp){
+    spindex[s,1]~dnorm(logI.raw[1],tau.spi)
+  }
+  
+  ########### Smoothing done here   #############
+  
+  beta[1]~dnorm(0,0.000001)
+  beta[2]~dnorm(0,0.000001)
+  #taueps~dgamma(0.000001,0.000001)
+  taub~dgamma(0.000001,0.000001)
+  for(k in 1:num.knots){b[k]~dnorm(0,taub)}
+  
+  for (t in 1:(nyears)){
+    logI.raw[t] <- m[t]
+    m[t] <- mfe[t]+mre[t]
+    mfe[t] <- beta[1] * X[t,1] + beta[2] * X[t,2]
+    for (k in 1:num.knots){
+      temp[t,k] <- b[k]*Z[t,k]
+      }
+    mre[t] <- sum(temp[t,1:num.knots])
+  }
+  
+  ###################  Define likelihood  #######################
+  
+  for (t in 1:(nyears-1)){
+    logLambda[t] <- logI.raw[t+1] - logI.raw[t]
+  }
+  
+  for (s in 1:nsp){
+    for (t in 1:(nyears-1)){
+      spgrowth[s,t]~dnorm(logLambda[t],tau.sg)
+    }
+  }
+  
+  for (s in 1:nsp){
+    for (t in 2:(nyears)){
+      spindex[s,t]<-spindex[s,1] + sum(spgrowth[s,1:(t-1)])
+    }
+  }
+  
+  for (s in 1:nsp){
+    for (t in 1:(nyears)){
+      estimate[s,t] ~ dnorm(spindex[s,t], tau.obs[s,t])
+      tau.obs[s,t] <- pow(sigma.obs[s,t], -2)
+    }
+  }
+  
+  logI <- m
+  
+  #########################  end likelihood ###########################
+
+}
+  '
+  
+  writeLines(text = model, con = temp_file)
+  return(temp_file)
+}
+
+################################################################################
