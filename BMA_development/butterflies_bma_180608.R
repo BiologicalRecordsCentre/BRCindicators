3a34da30dc082a4f3aaf375e5e14a79bb3b745c1
drnickisaac <njbisaac@gmail.com>
2018-06-11 10:40:33 +0100

    modified the script to loop through different scaling factors. Also streamlined the code.

--- BMA_development/butterflies_bma_180608.R
+++ BMA_development/butterflies_bma_180608.R
@@ -44,48 +44,47 @@ scaling_factor = 5 # inflate the standard errors
 #load(file.path(dir, 'butterfly_data.rdata'))
 
 # the 2017 data is stored locally
-new_raw <- read.csv('data/all_spp_ci.csv')
+raw_data <- read.csv('data/all_spp_ci.csv')
 all_species <- read.csv(file='data/all_species.csv')
 
-head(new_raw)
+head(raw_data)
 head(all_species)
 
+##################################################### SETUP
+#```{r setup dataset, results = 'hide'}
 
-## Wider countryside
-
-#Let's first run models with the wider countryside species.
-
-#```{r wider, results = 'hide'}
-## Wider
-in_data <- subset(new_raw, SPECIES %in% all_species[all_species$type=='wider', 'BMScode'])
-
-in_data <- in_data[,c('SPECIES','YEAR','TR0OBS','TR0SE')] 
-
+in_data <- raw_data[,c('SPECIES','YEAR','TR0OBS','TR0SE')] 
 names(in_data) <- c('species','year','index','se')
-nrow(in_data) # 1007
-
-# rescale for BMA
-firstyr <- min(new_raw$YEAR)
-
-length(unique(in_data$species))
-
-# start the years at 1
-in_data$year <- in_data$year - firstyr + 1
-#```
-
-#Now we can run the models.
-
-#```{r runButterflies_wider, echo = FALSE, message = FALSE, fig.width = 6, fig.height = 4, warning = FALSE, results = 'hide'}
-group <- 'wider'
+nrow(in_data)
 
 in_data$se[in_data$se == 0] <- NA 
 # SEs are on the natural log scale so change them to the log10 scale
 #in_data$se <- in_data$se/log(10) NO - they are acutally on the correct scale
-in_data$se <- in_data$se * scaling_factor
 
-for(nk in c(4, 12)){
-  
-bma_indicator <- bma(in_data, model = 'smooth_det2',
+# rescale year
+firstyr <- min(in_data$year)
+length(unique(in_data$species))
+# start the years at 1
+in_data$year <- in_data$year - firstyr + 1
+#```
+
+#```{r setup loops}
+for(nk in c(4, 12, 20)){
+  for(scaling_factor in c(1,5,10)){
+    in_data$se <- in_data$se * scaling_factor
+
+#```    
+    
+##################################################### WIDER
+#Now we can run the models.
+
+#```{r runButterflies_wider, echo = FALSE, message = FALSE, fig.width = 6, fig.height = 4, warning = FALSE, results = 'hide'}
+
+w_data <- subset(in_data, species %in% all_species[all_species$type=='wider', 'BMScode'])
+group <- 'wider'
+message <- paste0(': ', nk,' knots; se scale factors=', scaling_factor)
+
+bma_indicator <- bma(w_data, model = 'smooth_det2',
                      n.iter = ifelse(test, 100, iter),
                      parallel = TRUE,
                      m.scale = 'log10',
@@ -94,61 +93,33 @@ bma_indicator <- bma(in_data, model = 'smooth_det2',
 
 plot_indicator(indicator = bma_indicator[,'Index'],
                CIs = bma_indicator[,c(3,4)], 
-               main = paste0(group, ' Butterflies - smoothdet Indicator', nk,' knots'))
+               main = paste(group, 'indicator', message))
 
 model <- attr(bma_indicator, 'model')
 plot_growthrates(logLambda = model$mean$logLambda,
                  CIs = with(model, 
                             data.frame(lower=q2.5$logLambda, upper=q97.5$logLambda)),
-                 main = paste0(group, ' Butterflies - smoothdet Growth rates', nk,' knots'))
+                 main = paste(group,'growth rates', message))
 
 # plot the modelled vs observed spindex
-match_data <- in_data
+match_data <- w_data
 spmatch <- data.frame(bms=unique(match_data$species), model=order(unique(match_data$species)))
 match_data$species <- spmatch$model[match(match_data$species, spmatch$bms)]
 
 plot_species(spindex = model$mean$spindex,
              lower = model$q2.5$spindex,
              upper = model$q97.5$spindex,
-             data_in = match_data, main = paste(group, nk, 'knots'))
-
-}
-
-## Specialists
-
-#Lets now run this with the specialist butterflies.
-
-#```{r specialists, results = 'hide'}
-## Specialists
-in_data <- subset(new_raw, SPECIES %in% all_species[all_species$type=='specialist', 'BMScode'])
-in_data <- in_data[,c('SPECIES','YEAR','TR0OBS','TR0SE')] 
+             data_in = match_data, main = paste(group, message))
 
 
-names(in_data) <- c('species','year','index','se')
-nrow(in_data) # 943
-
-# rescale for BMA
-firstyr <- min(new_raw$YEAR)
-
-length(unique(in_data$species)) # 11 or 26, depending on whether the first year is to be included
-
-# start the years at 1
-in_data$year <- in_data$year - firstyr + 1
-#```
-
-#Now we can run the models for specialist butterflies.
+###################################################### specialists
+##Now we can run the models for specialist butterflies.
 
 #```{r runButterflies_specialists, echo = FALSE, message = FALSE, fig.width = 6, fig.height = 4, warning = FALSE, results = 'hide'}
+s_data <- subset(in_data, species %in% all_species[all_species$type=='specialist', 'BMScode'])
 group <- 'specialists'
 
-in_data$se[in_data$se == 0] <- NA 
-# SEs are on the natural log scale so change them to the log10 scale
-#in_data$se <- in_data$se/log(10) NO - they are on the raw scale
-in_data$se <- in_data$se * scaling_factor
-
-for(nk in c(4, 12)){
-
-bma_indicator <- bma(in_data, model = 'smooth_det2',
+bma_indicator <- bma(s_data, model = 'smooth_det2',
                      n.iter = ifelse(test, 100, iter),
                      parallel = TRUE,
                      m.scale = 'log10',
@@ -157,24 +128,26 @@ bma_indicator <- bma(in_data, model = 'smooth_det2',
 
 x<-plot_indicator(indicator = bma_indicator[,'Index'],
                CIs = bma_indicator[,c(3,4)],
-               main = paste0(group, ' Butterflies - smoothdet ', nk,' knots'))
+               main = paste(group,'indicator', message))
 
 model <- attr(bma_indicator, 'model')
 plot_growthrates(logLambda = model$mean$logLambda,
                  CIs = with(model, 
                             data.frame(lower=q2.5$logLambda, upper=q97.5$logLambda)),
-                 main = paste0(group, ' Butterflies - smoothdet Growth rates', nk,' knots'))
+                 main = paste(group,'growth rates', message))
 
 # plot the modelled vs observed spindex
-match_data <- in_data
+match_data <- s_data
 spmatch <- data.frame(bms=unique(match_data$species), model=order(unique(match_data$species)))
 match_data$species <- spmatch$model[match(match_data$species, spmatch$bms)]
 
 plot_species(spindex = model$mean$spindex,
              lower = model$q2.5$spindex,
              upper = model$q97.5$spindex,
-             data_in = match_data, main = paste(group, nk, 'knots'))
+             data_in = match_data, main = paste(group, message))
+
+###################################################### end loops
 }
-
-
+}
+###################################################### close
 dev.off()
