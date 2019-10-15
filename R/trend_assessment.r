bcc68f943e88cbef773ccb6a625c52389024d8bc
AugustT <tomaug@ceh.ac.uk>
2019-10-15 15:10:00 +0100

    Fix bug in trend_assessment

--- R/trend_assessment.r
+++ R/trend_assessment.r
@@ -24,13 +24,16 @@
 #' nyr = 40
 #' 
 #' #number of iterations
-#' iter = 3
+#' iter = 500
 #' 
 #' # Build a random set of data
-#' myArray <- array(rnorm(n = nsp*nyr*iter,
-#'                  mean = 0.5,
-#'                  sd = 0.2),
-#'                  c(nsp, nyr, iter))
+#' myArray <- array(data = rnorm(n = nsp*nyr*iter,
+#'                               mean = 0.5,
+#'                               sd = 0.1),
+#'                  dim = c(nsp, nyr, iter),
+#'                  dimnames = list(paste0('SP',1:nsp),
+#'                                  1:nyr,
+#'                                  1:iter))
 #' 
 #' # Ensure values are bounded by 0 and 1
 #' myArray[myArray > 1] <- 1
@@ -43,8 +46,8 @@
 #' trend_assessment(myIndicator)
 
 trend_assessment <- function(lambda_output,
-                             start_year = NULL,
-                             end_year = NULL,
+                             start_year = min(lambda_output$summary$year),
+                             end_year = max(lambda_output$summary$year),
                              species_stat = 'mean'){
   
   sp_assess <- species_assessment(LogLambda = lambda_output$LogLambda,
