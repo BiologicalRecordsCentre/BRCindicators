e809781533da4540121e790eaad01518ebef085a
drnickisaac <njbisaac@gmail.com>
2019-11-28 17:35:34 +0000

    updated documentation

--- R/bayesian_meta_analysis.R
+++ R/bayesian_meta_analysis.R
@@ -45,7 +45,7 @@
 #' # Create some example data in the format required
 #' data <- data.frame(species = rep(letters, each = 50),
 #'                    year = rep(1:50, length(letters)),
-#'                    index = logit(runif(n = 50 * length(letters), min = 0, max = 1)),
+#'                    index = rnorm(n = 50 * length(letters), mean = 0, sd = 1),
 #'                    se = runif(n = 50 * length(letters), min = 0.01, max = .1))
 #' 
 #' # Run the Bayesian meta-analysis
