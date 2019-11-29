ff3c11785ce7f03b65bd597d943b2e4de7a1350f
drnickisaac <njbisaac@gmail.com>
2019-11-29 18:45:49 +0000

    META-coding of BMA options

--- R/get_bmaBUGScode.R
+++ R/get_bmaBUGScode.R
@@ -7,15 +7,29 @@
 #' @export
 #' @examples
 #' get_bmaBUGScode(option="smooth", print.screen=TRUE, save.local=FALSE)
+#' get_bmaBUGScode(option="bma_model_smooth_det", print.screen=TRUE, save.local=FALSE)
+#' get_bmaBUGScode(option="bma_model_smooth_det2", print.screen=TRUE, save.local=FALSE)
+#' get_bmaBUGScode(option="bma_model_smooth_det_sigtheta", print.screen=TRUE, save.local=FALSE)
 
 get_bmaBUGScode <- function(option="smooth", 
-                                print.screen = FALSE,
-                                save.local = FALSE) {
+                            print.screen = FALSE,
+                            save.local = FALSE,
+                            incl.2deriv = FALSE, 
+                            seFromData = FALSE){
   
   switch(tolower(option),
-         smooth = {model_code <- bma_model_Smooth()},
-         smooth_det2 = {model_code <- bma_model_smooth_det2()},
-         smooth_det_sigtheta = {model_code <- bma_model_smooth_det_sigtheta()},
+         smooth = {model_code <- bma_model_Smooth(# this is the version SF ran for the paper
+                                                  incl.2deriv, 
+                                                  seFromData=FALSE,
+                                                  Y1perfect = TRUE)},
+         smooth_det2 = {model_code <- bma_model_Smooth(# this is the version NI tested for ISEC 
+                                                  incl.2deriv, 
+                                                  seFromData = TRUE,
+                                                  Y1perfect = FALSE)},
+         smooth_det_sigtheta = {model_code <- bma_model_Smooth(# this is the version NI ran for Scottish indicators
+                                                  incl.2deriv, 
+                                                  seFromData=FALSE,
+                                                  Y1perfect = FALSE)},
          smooth_det = {model_code <- bma_model_smooth_det()},
          random_walk = stop("Random walk model has been deprecated"),
          uniform = stop("Uniform model has been deprecated"),
@@ -28,7 +42,7 @@ get_bmaBUGScode <- function(option="smooth",
                      "'smooth_det', 'smooth_det2', 'smooth_det_sigtheta'"))})
 
   if(print.screen) cat(model_code, sep = '\n')
-  if(save.local) writeLines(text = model_code, con = "bma_BUGS_code.txt")
+  if(save.local) writeLines(text = model_code, con = paste0("bma_BUGS_code_",option,".txt"))
   return(model_code)
 }
 
