ef2a5718016b7a229607cbb7533aeef3ae20cb89
DylanCarbone <dylcar@ceh.ac.uk>
2024-03-22 10:45:04 +0000

    Fix of spelling mistake

--- R/bayesian_meta_analysis.R
+++ R/bayesian_meta_analysis.R
@@ -82,7 +82,7 @@ set.seed(seed = seed)
 
 # Check if jagsUI is installed
 if (!requireNamespace("jagsUI", quietly = TRUE)) {
-stop("Package 'jagsUI' is needed for the 'bma' function to work. Please insatll this from CRAN. You will also be required to install JAGS, which you can download from https://sourceforge.net/projects/mcmc-jags/files/JAGS/",
+stop("Package 'jagsUI' is needed for the 'bma' function to work. Please install this from CRAN. You will also be required to install JAGS, which you can download from https://sourceforge.net/projects/mcmc-jags/files/JAGS/",
       call. = FALSE)
 }
 
