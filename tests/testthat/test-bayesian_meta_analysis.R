33b80464bd77ad89fa3a3de57d38144144f7b68b
DylanCarbone <dylcar@ceh.ac.uk>
2024-04-17 16:52:14 +0100

    added skips for when there is no JAGS installation

--- tests/testthat/test-bayesian_meta_analysis.R
+++ tests/testthat/test-bayesian_meta_analysis.R
@@ -1,6 +1,5 @@
 test_that("Does the function stop when there is no jags installation?", {
-
-  # Mock `requireNamespace` to return FALSE
+  # Mock `detect_jags` to return FALSE
   mockery::stub(bma, "detect_jags", FALSE)
 
   expect_error(bma(bayesian_meta_analysis_fake_data), regexp = "No installation of JAGS has been detected")
@@ -8,50 +7,62 @@ test_that("Does the function stop when there is no jags installation?", {
 
 # Test that function responds correctly when 'jagsUI' is not available
 test_that("Does the function stop when 'jagsUI' is not available?", {
+  # Mock `detect_jags` to return TRUE
+  mockery::stub(bma, "detect_jags", TRUE)
 
   # Mock `requireNamespace` to return FALSE
   mockery::stub(bma, "requireNamespace", FALSE)
 
   expect_error(bma(bayesian_meta_analysis_fake_data), regexp = "Package 'jagsUI' is needed")
-
 })
 
 test_that("Does it correctly detect missing columns in the input data?", {
+  # skip the test if JAGS is not installed
+  if (!detect_jags()) {
+    skip("JAGS software not detectable")
+  }
 
-expect_error(bma(subset(bayesian_meta_analysis_fake_data, select = -c(index))),
- regexp = "data column names should include")
+  expect_error(bma(subset(bayesian_meta_analysis_fake_data, select = -c(index))),
+    regexp = "data column names should include"
+  )
 })
 
 test_that("Does it correctly detect a missing standard error column?", {
+  # skip the test if JAGS is not installed
+  if (!detect_jags()) {
+    skip("JAGS software not detectable")
+  }
 
-expect_error(bma(subset(bayesian_meta_analysis_fake_data, select = -c(se)), seFromData = TRUE),
- regexp = "Standard errors have not been provided")
+  expect_error(bma(subset(bayesian_meta_analysis_fake_data, select = -c(se)), seFromData = TRUE),
+    regexp = "Standard errors have not been provided"
+  )
 })
 
 test_that("Does it correctly return an error if a deprecated model, in this case smooth_det, is chosen?", {
+  # skip the test if JAGS is not installed
+  if (!detect_jags()) {
+    skip("JAGS software not detectable")
+  }
 
-expect_error(bma(bayesian_meta_analysis_fake_data, model = "smooth_det"),
- regexp = "model has been deprecated")
+  expect_error(bma(bayesian_meta_analysis_fake_data, model = "smooth_det"),
+    regexp = "model has been deprecated"
+  )
 })
 
-test_that("Does the function output, assuming 'jagsUI' is available, return a dataframe?", {
-
-    # skip the test if jagsUI is not installed
-    if (!detect_jags()) {
+test_that("Does the function output, assuming jags is available, return a dataframe?", {
+  # skip the test if JAGS is not installed
+  if (!detect_jags()) {
     skip("JAGS software not detectable")
   }
 
   expect_equal(class(suppressWarnings(bma(bayesian_meta_analysis_fake_data, plot = FALSE))), "data.frame")
-
 })
 
 test_that("Does the function return a Markov Chain Monte Carlo (MCMC) output object?", {
-
-    # skip the test if jagsUI is not installed
-    if (!detect_jags()) {
+  # skip the test if jagsUI is not installed
+  if (!detect_jags()) {
     skip("JAGS software not detectable")
   }
 
   expect_equal(class(suppressWarnings(bma_objects(bayesian_meta_analysis_fake_data, plot = TRUE)$comb.samples)), "mcmc.list")
-
 })
