084622707dc6fcb3f077856ac0f75e9514565532
DylanCarbone <dylcar@ceh.ac.uk>
2024-04-26 09:41:05 +0100

    Removed detect_jags from exported functions; Removed jags ui check in favour of detect_jags check

--- R/detect_jags.r
+++ R/detect_jags.r
@@ -1,13 +1,4 @@
-#' Detect JAGS installation
-#' 
-#' @description small function that detects whether JAGS is installed.
-#' @return TRUE or FALSE, indicating whether the JAGS installation has been detected 
-#' @importFrom runjags testjags
-#' @export
-#' @examples
-#' 
-#' detect_jags()
-
+# Internal function to detect JAGS installation.
 detect_jags <- function(){
     return(suppressWarnings(runjags::testjags(silent = TRUE)$JAGS.found))
 }
